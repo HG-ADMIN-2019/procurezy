7cccae77e7480a2e825026718543ffeec4bae831
shilpa <shilpa.ilkal@hiranya-garbha.com>
2023-03-24 09:41:02 +0530

    changes

--- eProc_Basic_Settings/static/scripts/upload-countries.js
+++ eProc_Basic_Settings/static/scripts/upload-countries.js
@@ -49,14 +49,18 @@ function onclick_copy_update_button(data) {
     $("#id_popup_tbody").empty();
     //Reference the Table.
     var grid = document.getElementById("display_basic_table");
+    var table = $('#display_basic_table').DataTable();
+    var $table = table.table().node();
+    var res = table.rows().nodes();
+     var $chkbox_all = $('td input[type="checkbox"]', res);
     //Reference the CheckBoxes in Table.
-    var checkBoxes = grid.getElementsByTagName("INPUT");
+//    var checkBoxes = grid.getElementsByTagName("INPUT");
     var edit_basic_data = "";
     var unique_input = '';
     //Loop through the CheckBoxes.
-    for (var i = 1; i < checkBoxes.length; i++) {
-        if (checkBoxes[i].checked) {
-            var row = checkBoxes[i].parentNode.parentNode;
+    for (var i = 1; i < $chkbox_all.length; i++) {
+        if ($chkbox_all[i].checked) {
+            var row = $chkbox_all[i].parentNode.parentNode;
             if(GLOBAL_ACTION == "UPDATE"){
                 unique_input = '<input class="form-control check_special_char" type="text" value="' + row.cells[1].innerHTML + '" name=""  maxlength="2" style="text-transform:uppercase" disabled>'
                 edit_basic_data += '<tr ><td hidden><input type="checkbox" required></td>'+
