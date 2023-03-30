4e05872fd8690efdc6f1921c100d18b1ba4fca98
chaitraps4 <chaitra@hiranya-garbha.com>
2023-03-30 14:53:36 +0530

    code merge

--- eProc_Master_Settings/Utilities/master_settings_specific.py
+++ eProc_Master_Settings/Utilities/master_settings_specific.py
@@ -74,6 +74,10 @@ class MasterSettingsSave:
                     django_query_instance.django_filter_delete_query(UnspscCategoriesCust,
                                                                      {'prod_cat_id': prodcat_detail['prod_cat_id'],
                                                                       'client': self.client})
+                if prodcat_detail['del_ind'] in ['1', True]:
+                    prod_cat = prodcat_detail['prod_cat_id']
+
+                    delete_prod_cat_image_to_db(prod_cat)
 
         if prodcat_db_list:
             bulk_create_entry_db(UnspscCategoriesCust, prodcat_db_list)
@@ -1286,9 +1290,9 @@ def get_unspsc_cat_cust_data():
         prod_cat['prod_cat_desc'] = ' '
         for product_cat in prod_cat_desc:
             if prod_cat['prod_cat_id'] == product_cat['prod_cat_id']:
-                 if product_cat['language_id'] == global_variables.GLOBAL_USER_LANGUAGE.language_id:
+                if product_cat['language_id'] == global_variables.GLOBAL_USER_LANGUAGE.language_id:
                     prod_cat['prod_cat_desc'] = product_cat['category_desc']
-                 break
+                break
 
     for prod in upload_cust_prod_catogories:
         if prod['prod_cat_desc'] is None:
@@ -1921,3 +1925,15 @@ def save_prod_cat_image_to_db(prod_cat, file_name, attached_file):
         'created_by': global_variables.GLOBAL_LOGIN_USERNAME,
         'del_ind': False
     })
+
+
+def delete_prod_cat_image_to_db(prod_cat):
+    if django_query_instance.django_existence_check(ImagesUpload,
+                                                    {'client': global_variables.GLOBAL_CLIENT,
+                                                     'image_id': prod_cat}):
+        django_query_instance.django_get_query(ImagesUpload,
+                                               {'client': global_variables.GLOBAL_CLIENT,
+                                                'image_id': prod_cat}).image_url.delete(save=True)
+        django_query_instance.django_filter_delete_query(ImagesUpload,
+                                                         {'client': global_variables.GLOBAL_CLIENT,'image_default': False,
+                                                          'image_id': prod_cat})
