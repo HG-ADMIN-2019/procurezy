3b65f07c4a1290f605d9578b98e84fa62aa260a1
Siddharth1097 <siddarth@majjaka.com>
2023-04-20 15:27:42 +0530

    fix

--- eProc_Supplier_Order_Management/views/supplier_order_management_views.py
+++ eProc_Supplier_Order_Management/views/supplier_order_management_views.py
@@ -14,9 +14,12 @@ def po_extract(request):
     if request.POST:
         pdfData = ''
         reader = ''
+
         directory = os.path.join(str(settings.BASE_DIR), 'media', 'pdf_read')
         for root, dirs, files in os.walk(directory):
             for file in files:
+                additional_info = []
+                cost_obj_data = []
                 file_path = os.path.join(directory, file)  # create path
                 pdfData = tabula.read_pdf(file_path, pages="all", output_format="json")
                 reader = PdfReader(file_path)
@@ -26,7 +29,7 @@ def po_extract(request):
                 text = extract_text.splitlines()
                 print(text)
                 header_detail = {}
-                get_po_data(text, header_detail)
+                header_detail,supplier_address = get_po_data(text, header_detail)
                 get_po_table_data(pdfData[0], header_detail)
                 # pdf_file = open(file_path, 'rb')  # open .csv file
                 # reader = PyPDF2.PdfFileReader(pdf_file)
@@ -43,10 +46,13 @@ def po_extract(request):
                                 if text_value.find("Additional comments on the line item") != -1:
                                     value = text_value.split('\r')
                                     print("additional info:", value)
+                                    additional_info.append({'item_num':count+1,'data':value})
                                 elif text_value.find("Cost Object") != -1:
                                     print("Cost Object:", text_value)
+                                    cost_obj_data.append({'item_num':count+1,'data':text_value})
                                 elif text_value.find("External Product ID") != -1:
                                     print("External Product ID:", text_value)
+                                    ext_prod_id = text_value
                                 else:
                                     text_value = text_value.replace('\r', '')
                                     print("item data:", text_value)
@@ -62,7 +68,8 @@ def get_po_data(text, header_detail):
     """
 
     """
-    supplier_address = {'address_details': '', 'invoice_address': '','delivery_address':''}
+    supplier_address = {'address_details': '', 'invoice_address': '', 'delivery_address': ''}
+    header_detail['invoicing_detail'] = ''
     index = 0
     flag = False
     buyer_flag = False
@@ -72,11 +79,11 @@ def get_po_data(text, header_detail):
     incoterm_flag = False
     payment_term_flag = False
     invoicing_detail_flag = False
+    goods_recep_flag = False
+    supplier_note_text_flag = False
     for count, po_data in enumerate(text):
         if po_data.find("PURCHASE ORDER NO") != -1:
             header_detail['doc_num'] = remove_space(po_data.split(':')[1])
-        if po_data == "Vendor":
-            flag = True
         if flag:
             if not po_data.find("Buyer") != -1:
                 supplier_address['address_details'] = supplier_address['address_details'] + ' ' + po_data
@@ -86,24 +93,44 @@ def get_po_data(text, header_detail):
                     supplier_address['address_details'] = supplier_address['address_details'] + ' ' + data[0]
                     buyer_flag = True
                     flag = False
+        if po_data == "Vendor":
+            flag = True
+        if incoterm_flag:
+            header_detail['incoterm'] = po_data.split('Terms of Payment')[0]
+            payment_term_flag = True
+            incoterm_flag = False
+        if po_data == 'Terms of Delivery (In accordance with INCOTERMS 2010)':
+            incoterm_flag = True
+            delivery_address_flag = False
         if delivery_address_flag:
             supplier_address['delivery_address'] = supplier_address['delivery_address'] + ' ' + po_data
+        if goods_recep_flag:
+            header_detail['goods_recep'] = po_data
+            goods_recep_flag = False
+        if po_data == 'Goods Receiver':
+            goods_recep_flag = True
+        if supplier_note_text_flag:
+            if not po_data == 'Order line no. Product no. Product Order quantity UOM Unit price excl.':
+                header_detail['supplier_note_text'] = po_data
+            supplier_note_text_flag = False
+        if po_data == 'Note(s) to Supplier':
+            supplier_note_text_flag = True
         if invoicing_detail_flag:
-            header_detail['invoicing_detail']= po_data
-            invoicing_detail_flag = False
+            if not po_data.find('Goods Marking'):
+                header_detail['invoicing_detail'] = header_detail['invoicing_detail'] + ' ' + po_data
+            else:
+                header_detail['invoicing_detail'] = header_detail['invoicing_detail'] + ' ' + po_data.split('Goods Marking')[0]
+                invoicing_detail_flag = False
         if po_data == 'Invoicing':
             invoicing_detail_flag = True
             payment_term_flag = False
         if payment_term_flag:
             header_detail['payment_term'] = po_data
-        if incoterm_flag:
-            header_detail['incoterm'] = po_data.split('Terms of Payment')[0]
-            payment_term_flag = True
-        if po_data == 'Terms of Delivery (In accordance with INCOTERMS 2010)':
-            incoterm_flag = True
+
         if pdf_invoice_address_flag:
             header_detail['pdf_invoice_address'] = po_data.split('Delivery Address')[0]
             delivery_address_flag = True
+            pdf_invoice_address_flag = False
         if po_data == 'PDF Invoice Address':
             invoice_address = False
             pdf_invoice_address_flag = True
@@ -113,7 +140,7 @@ def get_po_data(text, header_detail):
         if invoice_address:
             supplier_address['invoice_address'] = supplier_address['invoice_address'] + ' ' + po_data
 
-    return header_detail
+    return header_detail,supplier_address
 
 
 def get_po_table_data(po_table_details, header_detail):
