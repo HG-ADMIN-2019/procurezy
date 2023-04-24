13161c4d5f303b198e2400979d91da9c9b0fcbd0
shilpa <shilpa.ilkal@hiranya-garbha.com>
2023-04-24 12:04:43 +0530

    main merge

--- eProc_Users/static/scripts/user-search.js
+++ eProc_Users/static/scripts/user-search.js
@@ -7,8 +7,29 @@
    $('#email').val(localStorage.getItem("email"));
    $('#user_type').val(localStorage.getItem("user_type"));
    $('#employee_id').val(localStorage.getItem("employee_id"));
+//   $('#user_locked_id').val(localStorage.getItem("user_locked"));
+//   $('#pwd_locked_id').val(localStorage.getItem("pwd_locked"));
+//   $('#is_active_id').val(localStorage.getItem("is_active"));
+   const form = document.getElementById("search_form");
+//   form.addEventListener("submit", validateForm);
    table_sort_filter_basic("table_sort_filter_basic")
 });
+
+function validateForm(event) {
+    var temp = $('#username').val();
+    if($('#username').val() == ''){
+        if($('#first_name').val() == ''){
+            if($('#last_name').val() == ''){
+                $('#error_message_search').text("Please enter Username or First name or Last name");
+                document.getElementById("error_message_search").style.color = "Red";
+                $("#error_msg").css("display", "block")
+                CloseLoaderPopup();
+                event.preventDefault();
+                return false;
+            }
+        }
+    }
+}
 
 //onclick of upload button display id_data_upload popup and set GLOBAL_ACTION button value
 function onclick_upload_button() {
