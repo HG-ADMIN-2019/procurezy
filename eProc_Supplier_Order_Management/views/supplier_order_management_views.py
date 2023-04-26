9683eaf252dd1e3333d4ea4af3358af1a46d148b
Siddharth1097 <siddarth@majjaka.com>
2023-04-26 18:19:27 +0530

    fix

--- eProc_Supplier_Order_Management/views/supplier_order_management_views.py
+++ eProc_Supplier_Order_Management/views/supplier_order_management_views.py
@@ -6,16 +6,8 @@ from pypdf._reader import PdfReader
 import tabula
 from Majjaka_eProcure import settings
 from eProc_Basic.Utilities.functions.distinct_list import distinct_list
-from eProc_Basic.Utilities.functions.django_query_set import DjangoQueries
-from eProc_Basic.Utilities.functions.guid_generator import guid_generator
-from eProc_Basic.Utilities.functions.string_related_functions import remove_space
-from eProc_Basic.Utilities.global_defination import global_variables
 from eProc_Shopping_Cart.context_processors import update_user_info
-from eProc_Supplier_Order_Management.Utilities.supplier_order_management_specific import save_po_data, \
-    get_po_table_data, get_po_data
-from eProc_Supplier_Order_Management.models.supplier_order_management_models import SOMPoHeader
-
-
+from eProc_Supplier_Order_Management.Utilities.supplier_order_management_specific import *
 
 
 def po_extract(request):
@@ -29,13 +21,14 @@ def po_extract(request):
     if request.POST:
         pdfData = ''
         reader = ''
-        item_row_num = []
+
         po_item_data = []
         directory = os.path.join(str(settings.BASE_DIR), 'media', 'pdf_read')
         for root, dirs, files in os.walk(directory):
             for file in files:
                 additional_info = []
                 cost_obj_data = []
+                item_row_num = []
                 file_path = os.path.join(directory, file)  # create path
                 pdfData = tabula.read_pdf(file_path, pages="all", output_format="json")
                 reader = PdfReader(file_path)
@@ -53,6 +46,7 @@ def po_extract(request):
                 header_detail['total_value'] = total_detail[0]
                 header_detail['currency'] = total_detail[1]
                 item_num = 0
+                po_item_data  = []
                 for count, item_data in enumerate(data['data']):
                     if count != 0:
                         for text_data in item_data:
@@ -79,6 +73,7 @@ def po_extract(request):
                                     print("item data:", text_value)
                 item_row_num = distinct_list(item_row_num)
                 po_item_details = []
+                print("po_item_data:",po_item_data)
                 for count, row_num in enumerate(item_row_num):
                     item_list = []
                     for item_data in po_item_data:
@@ -96,7 +91,4 @@ def po_extract(request):
                'header_detail': header_detail,
                'supplier_address': supplier_address
                }
-    return render(request, 'Supplier_Order_Management/po_extract.html', context)
-
-
-
+    return render(request, 'Supplier_Order_Management/som_po_data.html', context)
