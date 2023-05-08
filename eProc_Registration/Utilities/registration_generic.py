09a4da89ac8f1c3ee7a864bd65e575abee28cfac
shilpa <shilpa.ilkal@hiranya-garbha.com>
2023-05-08 11:16:06 +0530

    po

--- eProc_Registration/Utilities/registration_generic.py
+++ eProc_Registration/Utilities/registration_generic.py
@@ -4,11 +4,13 @@ from eProc_Basic.Utilities.functions.django_query_set import DjangoQueries
 from eProc_Basic.Utilities.functions.encryption_util import encrypt
 from eProc_Basic.Utilities.functions.guid_generator import guid_generator
 from eProc_Basic.Utilities.functions.image_type_funtions import get_image_type
+from eProc_Basic.Utilities.functions.json_parser import JsonParser
 from eProc_Basic.Utilities.functions.messages_config import get_msg_desc, get_message_desc
 from eProc_Basic.Utilities.global_defination import global_variables
 from eProc_Basic.Utilities.messages.messages import MSG190, MSG191, MSG177
 from eProc_Configuration.models import OrgClients, ImagesUpload, SupplierMaster, Country, Currency, Languages
 
+JsonParser_obj = JsonParser()
 django_query_instance = DjangoQueries()
 
 
@@ -50,33 +52,34 @@ def save_supplier_data(request):
     """
     message = {}
     update_supplier_guid = ''
+    data = JsonParser_obj.get_json_from_req(request)
     supplier_details = {}
     status = request.POST.get('status')
-    supplier_details['supplier_id'] = request.POST.get('supplier_id')
-    supplier_details['registration_number'] = request.POST.get('registration_number')
-    supplier_details['supp_type'] = request.POST.get('supplier_type')
-    supplier_details['name1'] = request.POST.get('name1')
-    supplier_details['name2'] = request.POST.get('name2')
-    supplier_details['language_id'] = request.POST.get('language_id')
-    supplier_details['city'] = request.POST.get('city_id')
-    supplier_details['postal_code'] = request.POST.get('postal_code_id')
-    supplier_details['street'] = request.POST.get('street_id')
-    supplier_details['country_code'] = request.POST.get('country_code_id')
-    supplier_details['currency_id'] = request.POST.get('currency_id')
-    supplier_details['landline'] = request.POST.get('landline_id')
-    supplier_details['mobile_num'] = request.POST.get('mobile_num_id')
-    supplier_details['fax'] = request.POST.get('fax_id')
-    supplier_details['email'] = request.POST.get('email_id')
-    supplier_details['search_term1'] = request.POST.get('search_term1_id')
-    supplier_details['search_term2'] = request.POST.get('search_term2_id')
-    supplier_details['delivery_days'] = request.POST.get('working_days_id')
-    supplier_details['duns_number'] = request.POST.get('duns_number_id')
-    supplier_details['email1'] = request.POST.get('email1_id')
-    supplier_details['email2'] = request.POST.get('email2_id')
-    supplier_details['email3'] = request.POST.get('email3_id')
-    supplier_details['email4'] = request.POST.get('email4_id')
-    supplier_details['email5'] = request.POST.get('email5_id')
-    supplier_details['output_medium'] = request.POST.get('output_medium_id')
+    supplier_details['supplier_id'] = data['supplier_id']
+    supplier_details['registration_number'] = data['registration_number']
+    supplier_details['supp_type'] = data['supplier_type']
+    supplier_details['name1'] = data['name1']
+    supplier_details['name2'] = data['name2']
+    supplier_details['language_id'] = data['language_id']
+    supplier_details['city'] = data['city_id']
+    supplier_details['postal_code'] = data['postal_code_id']
+    supplier_details['street'] = data['street_id']
+    supplier_details['country_code'] = data['country_code_id']
+    supplier_details['currency_id'] = data['currency_id']
+    supplier_details['landline'] = data['landline_id']
+    supplier_details['mobile_num'] = data['mobile_num_id']
+    supplier_details['fax'] = data['fax_id']
+    supplier_details['email'] = data['email_id']
+    supplier_details['search_term1'] = data['search_term1_id']
+    supplier_details['search_term2'] = data['search_term2_id']
+    supplier_details['delivery_days'] = data['working_days_id']
+    supplier_details['duns_number'] = data['duns_number_id']
+    # supplier_details['email1'] = data['email1_id']
+    # supplier_details['email2'] = data['email2_id']
+    # supplier_details['email3'] = data['email3_id']
+    # supplier_details['email4'] = data['email4_id']
+    # supplier_details['email5'] = data['email5_id']
+    supplier_details['output_medium'] = data['output_medium_id']
     encrypted_supp = encrypt(supplier_details['supplier_id'])
     if status == 'UPDATE':
         if django_query_instance.django_existence_check(SupplierMaster,
@@ -90,9 +93,8 @@ def save_supplier_data(request):
             msgid = 'MSG177'
             error_msg = get_message_desc(msgid)[1]
 
-
             message['success'] = error_msg
-            return message,encrypted_supp
+            return message, encrypted_supp
     else:
         if django_query_instance.django_existence_check(SupplierMaster,
                                                         {'client': global_variables.GLOBAL_CLIENT,
@@ -100,7 +102,6 @@ def save_supplier_data(request):
                                                          'del_ind': False}):
             msgid = 'MSG190'
             error_msg = get_message_desc(msgid)[1]
-
 
             message['error'] = error_msg
             return message, encrypted_supp
@@ -128,10 +129,12 @@ def save_supplier_data(request):
         else:
             supplier_details['supp_guid'] = guid_generator()
             supplier_details['client'] = global_variables.GLOBAL_CLIENT
-            supplier_details['country_code'] = django_query_instance.django_get_query(Country, {'country_code': supplier_details['country_code']})
+            supplier_details['country_code'] = django_query_instance.django_get_query(Country, {
+                'country_code': supplier_details['country_code']})
             supplier_details['currency_id'] = django_query_instance.django_get_query(Currency, {
                 'currency_id': supplier_details['currency_id']})
-            supplier_details['language_id'] = django_query_instance.django_get_query(Languages, {'language_id': supplier_details['language_id']})
+            supplier_details['language_id'] = django_query_instance.django_get_query(Languages, {
+                'language_id': supplier_details['language_id']})
             django_query_instance.django_create_query(SupplierMaster,
                                                       supplier_details)
             msgid = 'MSG177'
