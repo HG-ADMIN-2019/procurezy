19ffaeb598ce13f1fe2a85f61ab6b76e6501ed71
babudn <babudn@gmail.com>
2023-05-25 10:29:35 +0530

    fix

--- eProc_Configuration_Check/Utilities/configuration_check_generic.py
+++ eProc_Configuration_Check/Utilities/configuration_check_generic.py
@@ -1227,8 +1227,7 @@ def check_po_split_creteria_data(ui_data, status):
         if po_split['del_ind'] in ['1', True]:
             if status == 'SAVE':
                 if django_query_instance.django_existence_check(PoSplitCriteria,
-                                                                {'po_split_type': int(po_split[
-                                                                                          'po_split_type']),
+                                                                {'po_split_type': po_split['po_split_type'],
                                                                  'company_code_id':
                                                                      po_split['company_code_id'],
                                                                  'client': global_variables.GLOBAL_CLIENT
@@ -1237,8 +1236,7 @@ def check_po_split_creteria_data(ui_data, status):
                     valid_data_list.append(po_split)
         else:
             if django_query_instance.django_existence_check(PoSplitCriteria,
-                                                            {'po_split_type': int(po_split[
-                                                                                      'po_split_type']),
+                                                            {'po_split_type': po_split['po_split_type'],
                                                              'company_code_id':
                                                                  po_split['company_code_id'],
                                                              'client': global_variables.GLOBAL_CLIENT
@@ -1280,7 +1278,7 @@ def check_purchase_control_data(ui_data, status):
         if po_split['del_ind'] in ['1', True]:
             if status == 'SAVE':
                 if django_query_instance.django_existence_check(PurchaseControl,
-                                                                {'call_off': int(po_split['call_off']),
+                                                                {'call_off': po_split['call_off'],
                                                                  'company_code_id':
                                                                      po_split['company_code_id'],
                                                                  'client': global_variables.GLOBAL_CLIENT
@@ -1289,8 +1287,7 @@ def check_purchase_control_data(ui_data, status):
                     valid_data_list.append(po_split)
         else:
             if django_query_instance.django_existence_check(PurchaseControl,
-                                                            {'call_off': int(po_split[
-                                                                                      'call_off']),
+                                                            {'call_off': po_split['call_off'],
                                                              'company_code_id':
                                                                  po_split['company_code_id'],
                                                              'client': global_variables.GLOBAL_CLIENT
