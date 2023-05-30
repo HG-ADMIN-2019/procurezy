37a295bc3f12c21872c223931d4d1b517b251723
chaitraps4 <chaitra@hiranya-garbha.com>
2023-05-30 18:02:41 +0530

    code merge

--- Majjaka_eProcure/settings.py
+++ Majjaka_eProcure/settings.py
@@ -162,7 +162,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Database engine
         'HOST': 'sg2plzcpnl489587.prod.sin2.secureserver.net',  # Database host
-        'NAME': 'MEP_DEPLOYED_NEW_QA_26',  # Database name MEP_FIXING MEP_DEPLOYED_NEW_22 MEP_DEPLOYED_NEW_29
+        'NAME': 'MEP_FIXING',  # Database name MEP_FIXING MEP_DEPLOYED_NEW_22 MEP_DEPLOYED_NEW_29
         'USER': 'MajjakaShopEProcure',  # Database user name
         'PASSWORD': 'Project@2019',  # Database credentials
         'PORT': '3306',  # Database port number
