ff9d0163f2692411ff4109c23e10d19608b4021b
girish yendamoori <sriharsha@majjaka.com>
2023-06-16 17:21:25 +0530

    fixes

--- eProc_Master_Settings/static/scripts/address_data/master_address_type.js
+++ eProc_Master_Settings/static/scripts/address_data/master_address_type.js
@@ -1,35 +1,39 @@
-var address_data = new Array();
-var validate_add_attributes = [];
+var addresstype_data = new Array();
 var main_table_low_value = [];
-var address={};
+var validate_add_attributes = [];
+var duplicate_entry = [];
+var addresstype={};
+
+//onclick of upload button display id_data_upload popup and set GLOBAL_ACTION button value
+function onclick_upload_button() {
+    GLOBAL_ACTION = "addresstype_upload"
+    $("#id_popup_tbody").empty();
+    $('#id_data_upload').modal('show');
+    document.getElementById('id_file_data_upload').value = "";
+}
 
 // on click copy icon display the selected checkbox data
 function onclick_copy_button() {
     GLOBAL_ACTION = "COPY"
-    onclick_copy_update_button("copy")
+    onclick_copy_update_button("COPY")
     document.getElementById("id_del_add_button").style.display = "block";
 }
 
 // on click update icon display the selected checkbox data to update
 function onclick_update_button() {
     GLOBAL_ACTION = "UPDATE"
-    onclick_copy_update_button("update")
+    onclick_copy_update_button("UPDATE")
     document.getElementById("id_del_add_button").style.display = "none";
 }
 
-// on click copy icon display the selected checkbox data
-function onclick_upload_button() {
-    GLOBAL_ACTION = "address_upload"
-    $('#id_data_upload').modal('show');
-    document.getElementById('id_file_data_upload').value = "";
-}
 //onclick of cancel empty the popup table body and error messages
 $(".remove_upload_data").click(() => {
     $("#id_error_msg").html("");
+    $("#id_popup_tbody").empty();
     $("#id_error_msg").empty();
-    $('#address_details').modal('hide');
+    $('#Adrs_Type_Modal').modal('hide');
     $("#id_error_msg").prop("hidden", true);
-    $("#id_error_msg_address").prop("hidden", true);
+    $("#id_error_msg_address_type").prop("hidden", true);
     $("#id_error_msg_description").prop("hidden", true);
     $("#id_error_msg_description_length").prop("hidden", true);
     $("#id_check_error_messages").prop("hidden", true);
@@ -37,149 +41,210 @@ $(".remove_upload_data").click(() => {
     $("#id_check_special_character_messages").prop("hidden", true)
     $("#id_check_data").prop("hidden", true);
     $('#id_popup_table').DataTable().destroy();
+
+
 });
 
-// on click edit icon display the data in edit mode
-function onclick_edit_button() {
-    //display the add,cancel and upload buttons and select all checkbox,select heading and checkboxes for each row
-    $('#display_basic_table').DataTable().destroy();
-    $("#hg_select_checkbox").prop("hidden", false);
-    $(".class_select_checkbox").prop("hidden", false);
-    //hide the edit,delete,copy and update buttons
-    $('#id_cancel_data').show();
-    $('#id_edit_data').hide();
-    $('#id_check_all').show();
-    table_sort_filter('display_basic_table');
-}
-
-//**************************************************
+//*********************************************
 function display_error_message(error_message){
     $('#error_message').text(error_message);
     document.getElementById("error_message").style.color = "Red";
     $("#error_msg_id").css("display", "block")
     $('#id_save_confirm_popup').modal('hide');
-    $('#address_add_details').modal('show');
+    $('#Adrs_Type_Modal').modal('show');
 }
 
-// Function to hide and display save related popups
-$('#save_id').click(function () {
-        var isValid = save_user_form_validation();
+// on click add icon display the row in to add the new entries
+function add_popup_row() {
+    $("#error_msg_id").css("display", "none")
+    basic_add_new_html = '';
+    var display_db_data = '';
+    $('#id_popup_table').DataTable().destroy();
+    $(".modal").on("hidden.bs.modal", function () {
+        $("#id_error_msg").html("");
+    });
+    new_row_data();   // Add a new row in popup
+    if (GLOBAL_ACTION == "addresstype_upload") {
+        $(".class_del_checkbox").prop("hidden", false);
+    }
+}
 
-    if (isValid) {
-        $('#address_add_details').modal('hide');
-        $('#id_popup_table').DataTable().destroy();
-        validate_add_attributes = [];
-        address_data = new Array();
-        var user_data_dict = {};
-    $('#address_add_details').modal('hide');
+//onclick of cancel display the table in display mode............
+function display_basic_db_data() {
+    $('#display_basic_table').DataTable().destroy();
+    $('#id_address_type_tbody').empty();
+    var edit_basic_data = '';
+
+    $.each(rendered_address_type_data, function (i, item) {
+        var validFromDate = item.valid_from.split('T')[0].split('-').reverse().join('-');
+        var validToDate = item.valid_to.split('T')[0].split('-').reverse().join('-');
+
+        edit_basic_data += '<tr><td class="class_select_checkbox"><input class="checkbox_check" onclick="valueChanged()" type="checkbox" required></td>' +
+        '<td>'+ item.company_id +'</td>'+
+        '<td>' + item.address_type + '</td>' +
+        '<td>' + item.address_number + '</td>' +
+        '<td>'+ validFromDate +'</td>'+
+        '<td>'+ validToDate +'</td>'+
+        '<td hidden> <input type="checkbox"></td>' +
+        '<td hidden>' + item.address_guid + '</td></tr>';
+    });
+
+    $('#id_address_type_tbody').append(edit_basic_data);
+    $("#hg_select_checkbox").prop("hidden", true);
+    $(".class_select_checkbox").prop("hidden", true);
+    $('input:checkbox').removeAttr('checked');
+    $('#id_edit_data').show();
+    $('#id_cancel_data').hide();
+    $('#id_delete_data').hide();
+    $('#id_copy_data').hide();
+    $('#id_update_data').hide();
+    $('#id_save_confirm_popup').hide();
+    $('#id_delete_confirm_popup').hide();
+    $('#id_check_all').hide();
+    table_sort_filter('display_basic_table');
+}
+
+
+
+
+// Functtion to hide and display save related popups
+$('#save_id').click(function () {
+    $('#Adrs_Type_Modal').modal('hide');
+    addresstype_data = read_popup_data();
+    $('#id_save_confirm_popup').modal('show');
+});
+
+//Read popup table data
+function read_popup_data() {
     $('#id_popup_table').DataTable().destroy();
     validate_add_attributes = [];
-    address_data = new Array();
-    var user_data_dict = {}
-    user_data_dict.address_number = $('#id_add_addresses_address_number').val();
-    user_data_dict.title = $('#id_add_addresses_title').val();
-    user_data_dict.name1= $('#id_add_addresses_name1').val();
-    user_data_dict.name2= $('#id_add_addresses_name2').val();
-    user_data_dict.street= $('#id_add_addresses_street').val();
-    user_data_dict.area= $('#id_add_addresses_area').val();
-    user_data_dict.landmark=$('#id_add_addresses_landmark').val();
-    user_data_dict.city= $('#id_add_addresses_city').val();
-    user_data_dict.address_partner_type= $('#id_add_addresses_partnertype').val();
-    user_data_dict.org_address_source_system= $('#id_add_addresses_source').val();
-    user_data_dict.postal_code= $('#id_add_addresses_postalcode').val();
-    user_data_dict.region= $('#id_add_addresses_region').val();
-    user_data_dict.mobile_number= $('#id_add_addresses_mobile_number').val();
-    user_data_dict.telephone_number= $('#id_add_addresses_telephone_number').val();
-    user_data_dict.fax_number= $('#id_add_addresses_fax_number').val();
-    user_data_dict.email= $('#id_add_addresses_email').val();
-    user_data_dict.country_code= $('#id_add_addresses_country_code').val();
-    user_data_dict.language_id= $('#id_add_addresses_language_id').val();
-    user_data_dict.time_zone= $('#id_add_addresses_time_zone').val();
-    user_data_dict.del_ind= $('#id_add_addresses_del_ind_flag').val();
-    var user_data_dict_compare =  user_data_dict.address_number
-    if(user_data_dict.del_ind = 'false'){
-        user_data_dict.del_ind = 'False'
-    }
-    user_data_dict.address_guid= $('#id_add_addresses_address_guid').val();
-    if (user_data_dict == undefined) {
-        user_data_dict.address_number = $('#id_addresses_address_number').val();
-    }
-    if(user_data_dict.address_guid== undefined) {
-        user_data_dict.address_guid= ''
-    }
-    validate_add_attributes.push(user_data_dict_compare);
-    address_data.push(user_data_dict);
-    $('#id_save_confirm_popup').modal('show');
+    addresstype_data = new Array();
+    var check_dates = []
+    $("#id_popup_table TBODY TR").each(function () {
+        var row = $(this);
+        addresstype = {};
+        addresstype.del_ind = row.find("TD").eq(0).find('input[type="checkbox"]').is(':checked');
+        addresstype.address_guid = row.find("TD").eq(7).find('input').val();
+        addresstype.address_number = row.find("TD").eq(3).find('select').val();
+        addresstype.address_type = row.find("TD").eq(2).find('select').val();
+        addresstype.company_id = row.find("TD").eq(1).find('select').val();
+        addresstype.valid_from = row.find("TD").eq(4).find('input[type="text"]').val();
+        addresstype.valid_to = row.find("TD").eq(5).find('input[type="text"]').val();
+//        addresstype.valid_to = row.find("TD").eq(5).find('input[type="date"]').val();
+        var addresstype_compare = addresstype.address_number + '-' + addresstype.address_type + '-' + addresstype.company_id;
+        if (addresstype == undefined) {
+            addresstype.address_number = row.find("TD").eq(2).find('input').val();
+        }
+        if (addresstype.address_guid == undefined) {
+            addresstype.address_guid = '';
+        }
+        check_dates.push([addresstype.valid_from, addresstype.valid_to]);
+        validate_add_attributes.push(addresstype_compare);
+        addresstype_data.push(addresstype);
+    });
     table_sort_filter('id_popup_table');
-});
+    return addresstype_data;
+}
+
+// Function for add a new row data
+function new_row_data() {
+    basic_add_new_html = '<tr><td><input type="checkbox" required></td>' +
+        '<td><select class="form-control">'+company_dropdwn+'</select></td>' +
+        '<td><select class="form-control">'+address_type_dropdown+'</select></td>' +
+        '<td><select class="form-control">'+address_number_dropdwn+'</select></td>' +
+        '<td><input  type="text" name = "valid_from" class="form-control formatDate"></td>' +
+        '<td><input type="text" name = "valid_to"  class="form-control formatDate"></td>' +
+        '<td class="class_del_checkbox" hidden><input type="checkbox" required></td>' +
+        '<td hidden><input  type="text" class="form-control"  name="guid"></td></tr>';
+    $('#id_popup_tbody').append(basic_add_new_html);
+    DatePicker();
+    table_sort_filter('id_popup_table');
+}
 
 // Function to get main table data
 function get_main_table_data() {
     main_table_low_value = [];
     $('#display_basic_table').DataTable().destroy();
-    $.each(rendered_address_data, function (i, item) {
+    $("#display_basic_table TBODY TR").each(function () {
+        var row = $(this);
         var main_attribute = {};
-        main_attribute.address_number = item.address_number;
-        var main_compare =  main_attribute.address_number.toString();
-        main_table_low_value.push(main_compare);
+        main_attribute.address_number = row.find("TD").eq(3).html();
+        main_attribute.address_type = row.find("TD").eq(2).html();
+        main_attribute.company_id = row.find("TD").eq(1).html();
+        var address_compare_maintable = main_attribute.address_number +'-'+ main_attribute.address_type+'-'+main_attribute.company_id
+        main_table_low_value.push(address_compare_maintable);
     });
     table_sort_filter('display_basic_table');
 }
 
-// Validation function
-function save_user_form_validation(){
-   var is_valid = true;
-        var save_form_errors = ''
-        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-        var err_text1 = '';
-        var temp = document.getElementsByClassName('mandatory_fields');
-        for (var i = 0; i<temp.length; i++) {
-            if(temp[i].nodeName == "SELECT"){
-                if((temp[i].value == '') || (temp[i].value == null)){
-                    err_text1 = temp[i].parentNode.children[0].innerHTML;
-                    $('#temp[i].nextElementSibling.id').prop('hidden', false);
-                    $('#temp[i].nextElementSibling.id').html("required");
-                    document.getElementById(temp[i].nextElementSibling.id).innerHTML = err_text1 + " required";
-                    is_valid = false;
-                }
-                else{ $('#temp[i].nextElementSibling.id').prop('hidden', true);
-                }
-            }
-            else{
-                if(temp[i].value == ''){
-                    var err_text = temp[i].parentNode.children[0].innerHTML;
-                    $(".error_message").prop("hidden", false);
-                    temp[i].nextElementSibling.innerHTML = err_text + " required";
-                   is_valid = false;
-                }
-                 else if(temp[i].value.length < 3){
-                    var err_text = temp[i].parentNode.children[0].innerHTML;
-                    $(".error_message").prop("hidden", false);
-                    var display_id = temp[i].nextElementSibling.id;
-                    $('#'+display_id).prop('hidden', false);
-                    document.getElementById(display_id).style.display = "block";
-                    temp[i].nextElementSibling.innerHTML = "Please enter min 3 chars for "+ err_text;
-                    $('#'+temp[i].id).prop("disabled", false);
-                   is_valid = false;
-                }
-                else if(temp[i].id == 'email_id'){
-                    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-                     if (!(temp[i].value).match(mailformat)) {
-                            valid_data = false
-                             var msg = "JMSG002";
-                             var msg_type ;
-                             msg_type = message_config_details(msg);
-                             var display1 = msg_type.messages_id_desc;
-                             $(".error_message").prop("hidden", false);
-                            var display_id = temp[i].nextElementSibling.id;
-                            $('#'+display_id).prop('hidden', false);
-                            document.getElementById(display_id).style.display = "block";
-                            temp[i].nextElementSibling.innerHTML = display1 + " for Email Id";
-                           is_valid = false;
-                     }
-                }
-            }
+ // Function to get the selected row data
+ function get_selected_row_data() {
+    $("#display_basic_table TBODY TR").each(function () {
+        var row = $(this);
+        var address_type_arr_obj = {};
+        address_type_arr_obj.del_ind = row.find("TD").eq(0).find('input[type="checkbox"]').is(':checked');
+        if(address_type_arr_obj.del_ind ){
+            address_type_arr_obj.address_guid = row.find("TD").eq(7).html();
+             address_type_arr_obj.valid_to = row.find("TD").eq(5).html();
+              address_type_arr_obj.valid_from = row.find("TD").eq(4).html();
+             address_type_arr_obj.company_id = row.find("TD").eq(1).html();
+            address_type_arr_obj.address_type = row.find("TD").eq(2).html();
+            address_type_arr_obj.address_number = row.find("TD").eq(3).html();
+            main_table_address_type_checked.push(address_type_arr_obj);
         }
-        return is_valid
-    }
+    });
+ }
+function check_date(addresstype_data) {
+    var validDate = 'Y';
+    var error_message = ''
+    $.each(addresstype_data, function (i, item) {
+var validFromParts = item.valid_from.split('-');
+        var validToParts = item.valid_to.split('-');
+        var validFrom = new Date(validFromParts[2], validFromParts[1] - 1, validFromParts[0]);
+        var validTo = new Date(validToParts[2], validToParts[1] - 1, validToParts[0]);
+        var formattedValidFrom = validFrom.toLocaleDateString('en-GB');
+        var formattedValidTo = validTo.toLocaleDateString('en-GB');
+
+        if (validFrom > validTo) {
+            $("#id_error_msg").prop("hidden", false);
+            var msg = "JMSG017";
+            var msg_type = message_config_details(msg);
+            $("#error_msg_id").prop("hidden", false);
+
+            if (msg_type.message_type == "ERROR") {
+                display_message("error_msg_id", msg_type.messages_id_desc);
+            } else if (msg_type.message_type == "WARNING") {
+                display_message("id_warning_msg_id", msg_type.messages_id_desc);
+            } else if (msg_type.message_type == "INFORMATION") {
+                display_message("id_info_msg_id", msg_type.messages_id_desc);
+            }
+
+            var display = msg_type.messages_id_desc;
+            $('#id_save_confirm_popup').modal('hide');
+//            onclick_copy_update_button(item.account_assign_value);
+            $('#Adrs_Type_Modal').modal('show');
+            validDate = 'N';
+        }
+    });
+    return [validDate, error_message];
+}
+
+
+//*******************************************
+function check_date_error(check_dates) {
+    date_error = "N"
+    $.each(check_dates, function(index, value){
+        var d1=new Date(value[0]); //yyyy-mm-dd
+        var d2=new Date(value[1]); //yyyy-mm-dd
+        if (d2< d1) {
+            get_message_details("JMSG017"); // Get message details
+            var display = msg_type.messages_id_desc;
+            document.getElementById("id_error_msg").innerHTML = display;
+            document.getElementById("id_error_msg").style.color = "Red";
+            $('#id_save_confirm_popup').modal('hide');
+            $('#myModal').modal('show');
+            date_error = 'Y'
+        }
+    })
+    return date_error
+}
