2d6789296d360c6cc10af937fc5f075d53bb5c26
shilpa <shilpa.ilkal@hiranya-garbha.com>
2023-08-24 15:32:33 +0530

    new db

--- Majjaka_eProcure/settings.py
+++ Majjaka_eProcure/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '-p^#04gmm3jl$93p3jx9rm+t1nj4$xl9b_#%192&e_usmw*&nr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.majjaka-eprocure.com', 'majjaka-eprocure.com'] # for deployment
+ALLOWED_HOSTS = ['www.majjaka-eprocure.com', 'majjaka-eprocure.com']  # for deployment
 # ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -160,7 +160,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Database engine
         'HOST': 'sg2plzcpnl489587.prod.sin2.secureserver.net',  # Database host
-        'NAME': 'MEP_MIGRATE_DEV_NEW',  # Database name MEP_MIGRATE_DEV MEP_FIXING MEP_DEPLOYED_NEW_QA_23 MEP_DEPLOYED_NEW_29 MEP_DEPLOYED_NEW_QA_0506
+        'NAME': 'MEP_DEMO_NEW',
+        # Database name MEP_MIGRATE_DEV MEP_FIXING MEP_DEPLOYED_NEW_QA_23 MEP_DEPLOYED_NEW_29 MEP_DEPLOYED_NEW_QA_0506
         'USER': 'MajjakaShopEProcure',  # Database username
         'PASSWORD': 'Project@2019',  # Database credentials
         'PORT': '3306',  # Database port number
